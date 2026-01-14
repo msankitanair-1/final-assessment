e7d3d2375b80e829a47835b28b6e6a1b051bb68a
msankitanair-1 <ms.ankitanair@gmail.com>
2026-01-14 15:37:35 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import AddIcon from "@mui/icons-material/Add";
@@ -9,18 +10,31 @@ const Navbar = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: "#9c27b0" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Typography variant="h6">EmployeeApp</Typography>
+        
+        {/* Left Title */}
+        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+          EmployeeApp
+        </Typography>
+
+        {/* Right Icons */}
         <div>
           <IconButton color="inherit" onClick={() => navigate("/")}>
             <HomeIcon />
           </IconButton>
+
           <IconButton color="inherit" onClick={() => navigate("/add")}>
             <AddIcon />
           </IconButton>
         </div>
+
       </Toolbar>
     </AppBar>
   );
 };
 
 export default Navbar;
+
+
+
+
+
